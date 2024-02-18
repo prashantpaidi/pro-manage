876a95dc14598cd20bd54a76e852f57d398201e0
prashantpaidi <88718449+prashantpaidi@users.noreply.github.com>
2024-02-18 10:03:25 +0530

    Add initial React and Express boilerplate

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+
+dotenv.config();
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/health', (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date(),
+  };
+
+  res.status(200).send(data);
+});
+
+app.get('/', (req, res) => {
+  res.send('This is api for the Pro Manage app');
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() =>
+      console.log(`Server running on http://localhost:${process.env.PORT}`)
+    )
+    .catch((error) => console.error(error));
+});
